55fd6546f073fc286cd0fe5fc82ce5cc0aa43c3c
yunusemreyakisan <yunusemreyakisan@outlook.com>
2022-02-06 16:20:26 +0300

    Day 7

---
+++ Numpy/numpy.py
@@ -0,0 +1,21 @@
+'''NumPy bir Python kütüphanesidir. NumPy dizilerle çalışmak için kullanılır. NumPy, "Sayısal Python"un kısaltmasıdır.'''
+#Numpy Amacı:
+#Python'da dizilerin amacına hizmet eden listelerimiz var, ancak işlenmesi yavaş.
+#NumPy, geleneksel Python listelerinden 50 kata kadar daha hızlı bir dizi nesnesi sağlamayı amaçlar.
+
+'''NumPy Neden Listelerden Daha Hızlı?'''
+#NumPy dizileri, listelerin aksine bellekte tek bir sürekli yerde depolanır, böylece işlemler bunlara çok verimli bir şekilde erişebilir ve işleyebilir.
+#Bu davranışa bilgisayar biliminde referans yeri denir.
+
+'''NumPy'nin Kurulumu'''
+#Bu komutu kullanarak yükleyin
+'''C:\Users\Your Name>pip install numpy'''
+
+
+
+
+
+
+
+
+
